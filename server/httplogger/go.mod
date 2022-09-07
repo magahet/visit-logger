b99b490e63154ad00cecd911b976e88425f73009
Gar Mendiola <magahet@users.noreply.github.com>
2022-09-07 16:32:31 -0700

    name based caches done. report done.

--- server/httplogger/go.mod
+++ server/httplogger/go.mod
@@ -3,6 +3,13 @@ module httplogger
 go 1.17
 
 require (
+	github.com/go-playground/locales v0.14.0 // indirect
+	github.com/go-playground/universal-translator v0.18.0 // indirect
+	github.com/go-playground/validator/v10 v10.11.0 // indirect
 	github.com/hashicorp/golang-lru v0.5.4 // indirect
+	github.com/leodido/go-urn v1.2.1 // indirect
 	github.com/mitchellh/hashstructure/v2 v2.0.2 // indirect
+	golang.org/x/crypto v0.0.0-20211215153901-e495a2d5b3d3 // indirect
+	golang.org/x/sys v0.0.0-20210806184541-e5e7981a1069 // indirect
+	golang.org/x/text v0.3.7 // indirect
 )
--- server/httplogger/main.go
+++ server/httplogger/main.go
@@ -3,12 +3,16 @@ package main
 
 import (
 	"encoding/json"
-	"io"
 	"log"
 	"net/http"
 	"time"
 
+	"github.com/go-playground/validator/v10"
 	lru "github.com/hashicorp/golang-lru"
+)
+
+const (
+	cacheSize = 100
 )
 
 func init() {
@@ -20,41 +24,62 @@ func init() {
 
 // Entry defines a log entry.
 type Entry struct {
-	// Logs Explorer allows filtering and display of this as `jsonPayload.component`.
-	Name     string
-	Title    string
-	Url      string
-	LastSeen time.Time
-	Count	int
+	Name     string    `json:"name"`
+	Title    string    `json:"title"`
+	Url      string    `json:"url"`
+	LastSeen time.Time `json:"lastSeen"`
+	Count    int       `json:"count"`
+}
+
+func (e Entry) String() string {
+	j, _ := json.Marshal(e)
+	return string(j)
 }
 
 type server struct {
-	caches map[string]*lru.Cache
+	caches   map[string]*lru.Cache
+	validate *validator.Validate
 }
 
 type Report struct {
-	Name string `json:"name"`
-	Entries string `json:"entries"`
+	Name    string   `json:"name"`
+	Entries []*Entry `json:"entries"`
 }
 
-func (s *server) generateReport() string {
-	entries := [s.cache.Len()]*Entry
-	for i, k := range s.cache.Keys() {
-		eInt, ok := s.cache.Peek(k)
+func (s *server) generateReport(name string) []byte {
+	cache, ok := s.caches[name]
+	if !ok {
+		j, _ := json.Marshal(map[string]string{"error": "No entries found for given name"})
+		log.Println("No entries found for given name:", name)
+		return j
+	}
+	entries := make([]*Entry, cache.Len())
+	for i, k := range cache.Keys() {
+		eInt, ok := cache.Peek(k)
 		if !ok {
+			log.Println("key not found in cache (should not happen)", k)
 			continue
 		}
 		e := eInt.(*Entry)
 		entries[i] = e
 	}
 	r := Report{name, entries}
-	return json.Marshal()
+	j, _ := json.Marshal(r)
+	return j
 }
 
 func (s *server) rootHandler(w http.ResponseWriter, r *http.Request) {
 	switch r.Method {
 	case "GET":
-		io.WriteString(w, s.generateReport())
+		name := r.URL.Query().Get("name")
+		if err := s.validate.Var(name, "required,lowercase,alpha,gte=1,lte=20"); err != nil {
+			j, _ := json.Marshal(map[string]string{"error": "name is not valid"})
+			w.Write(j)
+			return
+
+		}
+
+		w.Write(s.generateReport(name))
 	case "POST":
 		if r.Method == http.MethodOptions {
 			w.Header().Set("Access-Control-Allow-Origin", "*")
@@ -62,6 +87,7 @@ func (s *server) rootHandler(w http.ResponseWriter, r *http.Request) {
 			w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
 			w.Header().Set("Access-Control-Max-Age", "3600")
 			w.WriteHeader(http.StatusNoContent)
+			log.Println("Sending CORS headers")
 			return
 		}
 
@@ -73,37 +99,45 @@ func (s *server) rootHandler(w http.ResponseWriter, r *http.Request) {
 		var e Entry
 		err := json.NewDecoder(r.Body).Decode(&e)
 		if err != nil {
+			log.Println("Could not parse POST body")
 			return
 		}
 
 		if e.Url == "" || e.Title == "" || e.Name == "" {
+			log.Println("Some or all required fields are missing")
 			return
 		}
 
-		pInt, ok := s.cache.Get(e.Title)
+		c, ok := s.caches[e.Name]
+		if !ok {
+			c, _ = lru.New(cacheSize)
+			s.caches[e.Name] = c
+			log.Println("New cache created for:", e.Name)
+		}
+
+		pInt, ok := c.Get(e.Title)
 
 		// Entry exists in cache
 		if ok {
 			p := pInt.(*Entry)
 			p.LastSeen = time.Now()
 			p.Count += 1
-			s.cache.Add(p.Title, p)
+			c.Add(p.Title, p)
+			log.Println("Entry updated:", p)
 		} else {
 			e.Count = 1
 			e.LastSeen = time.Now()
-			s.cache.Add(e.Title, e)
+			c.Add(e.Title, &e)
+			log.Println("Entry added:", e)
 		}
 	}
 
 }
 
 func main() {
-	cache, err := lru.New(100)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	s := server{cache: cache}
+	c := make(map[string]*lru.Cache)
+	v := validator.New()
+	s := server{c, v}
 
 	http.HandleFunc("/", s.rootHandler)
 	http.ListenAndServe(":5000", nil)
