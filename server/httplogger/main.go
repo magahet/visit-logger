b2b2af7a804e8a2bea8d69b7d8b4a180f57a5ea4
Gar <magahet@gmail.com>
2022-09-07 22:33:58 +0000

    WIP multi-cache

--- server/httplogger/main.go
+++ server/httplogger/main.go
@@ -3,6 +3,7 @@ package main
 
 import (
 	"encoding/json"
+	"io"
 	"log"
 	"net/http"
 	"time"
@@ -24,42 +25,75 @@ type Entry struct {
 	Title    string
 	Url      string
 	LastSeen time.Time
+	Count	int
 }
 
 type server struct {
-	cache *lru.Cache
+	caches map[string]*lru.Cache
 }
 
-func (s *server) log(w http.ResponseWriter, r *http.Request) {
-	if r.Method == http.MethodOptions {
+type Report struct {
+	Name string `json:"name"`
+	Entries string `json:"entries"`
+}
+
+func (s *server) generateReport() string {
+	entries := [s.cache.Len()]*Entry
+	for i, k := range s.cache.Keys() {
+		eInt, ok := s.cache.Peek(k)
+		if !ok {
+			continue
+		}
+		e := eInt.(*Entry)
+		entries[i] = e
+	}
+	r := Report{name, entries}
+	return json.Marshal()
+}
+
+func (s *server) rootHandler(w http.ResponseWriter, r *http.Request) {
+	switch r.Method {
+	case "GET":
+		io.WriteString(w, s.generateReport())
+	case "POST":
+		if r.Method == http.MethodOptions {
+			w.Header().Set("Access-Control-Allow-Origin", "*")
+			w.Header().Set("Access-Control-Allow-Methods", "POST")
+			w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
+			w.Header().Set("Access-Control-Max-Age", "3600")
+			w.WriteHeader(http.StatusNoContent)
+			return
+		}
+
+		// Set CORS headers for the main request.
 		w.Header().Set("Access-Control-Allow-Origin", "*")
-		w.Header().Set("Access-Control-Allow-Methods", "POST")
-		w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
-		w.Header().Set("Access-Control-Max-Age", "3600")
-		w.WriteHeader(http.StatusNoContent)
-		return
+
+		r.Body = http.MaxBytesReader(w, r.Body, 4096)
+
+		var e Entry
+		err := json.NewDecoder(r.Body).Decode(&e)
+		if err != nil {
+			return
+		}
+
+		if e.Url == "" || e.Title == "" || e.Name == "" {
+			return
+		}
+
+		pInt, ok := s.cache.Get(e.Title)
+
+		// Entry exists in cache
+		if ok {
+			p := pInt.(*Entry)
+			p.LastSeen = time.Now()
+			p.Count += 1
+			s.cache.Add(p.Title, p)
+		} else {
+			e.Count = 1
+			e.LastSeen = time.Now()
+			s.cache.Add(e.Title, e)
+		}
 	}
-
-	// Set CORS headers for the main request.
-	w.Header().Set("Access-Control-Allow-Origin", "*")
-
-	if r.Method != "POST" {
-		return
-	}
-
-	r.Body = http.MaxBytesReader(w, r.Body, 4096)
-
-	var e Entry
-	err := json.NewDecoder(r.Body).Decode(&e)
-	if err != nil {
-		return
-	}
-
-	if e.Url == "" || e.Title == "" || e.Name == "" {
-		return
-	}
-
-	s.cache.Add(e.Title, e)
 
 }
 
@@ -71,6 +105,6 @@ func main() {
 
 	s := server{cache: cache}
 
-	http.HandleFunc("/", s.log)
+	http.HandleFunc("/", s.rootHandler)
 	http.ListenAndServe(":5000", nil)
 }
